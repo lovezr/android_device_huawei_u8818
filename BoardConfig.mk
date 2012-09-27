c06e1a5f67cd5b9e77d38e0b034d4ed1703da773
Liukai <liukai@shendu.com>
2012-09-27 17:50:42 +0800

    o fix wifi hostap o tweak vm o remove unuse parameter o fix typo in .conf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,6 @@ BOARD_PROVIDES_LIBRIL := true
 # Wi-Fi
 WIFI_BAND := 802_11_ABG
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-HOSTAPD_VERSION                  := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER := NL80211
@@ -90,10 +89,10 @@ BOARD_WLAN_DEVICE := bcmdhd
 WIFI_DRIVER_MODULE_PATH     := "/system/wifi/dhd_4330.ko"
 WIFI_DRIVER_MODULE_NAME     := "dhd"
 WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/wifi/fw_4330_b2.bin nvram_path=/system/wifi/nvram_4330.txt"
-WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+#WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/wifi/fw_4330_b2.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/wifi/fw_4330_b2.bin"
-WIFI_DRIVER_FW_PATH_P2P     := "/system/wifi/fw_4330_b2.bin"
+#WIFI_DRIVER_FW_PATH_P2P     := "/system/wifi/fw_4330_b2.bin"
 
 TARGET_CUSTOM_WIFI := ../../device/huawei/u8818/private/libhardware_legacy/wifi/wifi.c
 
